36659b62d306ff6ddd430b06620153e680a7e2e6
Arkadiusz Staśko <arek.stasko@gmail.com>
2022-04-30 18:15:01 +0200

    Add full algo tool

--- AlgorithmsEfficiency/Controllers.cs
+++ AlgorithmsEfficiency/Controllers.cs
@@ -8,50 +8,43 @@ namespace AlgorithmsEfficiency
 {
     internal class Controllers
     {
-        public List<int[]> RandomArrays = new List<int[]>();
-        public List<int[]> SortedArrays = new List<int[]>();
-        public List<int[]> ReversedArrays = new List<int[]>();
-        public List<int[]> AlmostSortedArrays = new List<int[]>();
-        public List<int[]> FewUniqueArrays = new List<int[]>();
+        public Dictionary<string, List<int[]>> DataArrays = new Dictionary<string, List<int[]>>();
 
+        
 
         public void InitializeArrays(int ElementsCount)
         {
-            Console.WriteLine("Initialize array with custom numbers...");
+            DataArrays.Add("RandomArrays", new List<int[]> { });
+            DataArrays.Add("SortedArrays", new List<int[]> { });
+            DataArrays.Add("ReversedArrays", new List<int[]> { });
+            DataArrays.Add("AlmostSortedArrays", new List<int[]> { });
+            DataArrays.Add("FewUniqueArrays", new List<int[]> { });
 
-            for(int i = 0; i < 3; i++)
-                RandomArrays.Add(GetRandomNumbersArray(ElementsCount));
-
-            Console.WriteLine("Initialize array with sorted numbers...");
 
             for (int i = 0; i < 3; i++)
-                SortedArrays.Add(GetSortedArray(ElementsCount));
+                DataArrays["RandomArrays"].Add(GetRandomNumbersArray(ElementsCount));
 
-            Console.WriteLine("Initialize array with reversed numbers...");
-            
-            for(int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
+                DataArrays["SortedArrays"].Add(GetSortedArray(ElementsCount));
+
+
+            for (int i = 0; i < 3; i++)
             {
                 int[] arr = GetSortedArray(ElementsCount);
                 Array.Reverse(arr);
-                ReversedArrays.Add(arr);
+                DataArrays["ReversedArrays"].Add(arr);
             }
-
-            Console.WriteLine("Initialize array with almost sorted elements...");
 
             for(int i = 0; i <3; i++)
             {
                 int[] almostSortedArray = GetSortedArray(ElementsCount)[0..(ElementsCount / 2)];
                 int[] randomizedArray = GetRandomNumbersArray(ElementsCount)[(ElementsCount / 2)..ElementsCount];
                 int[] arr = almostSortedArray.Concat(randomizedArray).ToArray();
-                AlmostSortedArrays.Add(arr);
+                DataArrays["AlmostSortedArrays"].Add(arr);
             }
 
-            Console.WriteLine("Initialize array with few unique elements...");
-
             for(int i = 0; i < 3; i++)
-                FewUniqueArrays.Add(GetArrWithUniqueElem(ElementsCount));
-
-            Console.WriteLine("End of initializing data");
+                DataArrays["FewUniqueArrays"].Add(GetArrWithUniqueElem(ElementsCount));
         }
 
         private int[] GetArrWithUniqueElem(int ElementsCount)
--- AlgorithmsEfficiency/Presenters.cs
+++ AlgorithmsEfficiency/Presenters.cs
@@ -9,50 +9,86 @@ namespace AlgorithmsEfficiency
 {
     internal class Presenters
     {
+        private Controllers _controllers = new Controllers();
+        private int count = 0;
         private List<double> InsertionSort { get; set; } = new List<double> { };
         private List<double> MergeSort { get; set; } = new List<double> { };
         private List<double> QuickSortClassical { get; set; } = new List<double> { };
         private List<double> QuickSort { get; set; } = new List<double> { };
 
 
+
         public void PrintStatistics()
         {
-            RunAlgo();
+            Dictionary<string, int> elements = new Dictionary<string, int>
+            {
+                {"mala", 10 },
+                {"srednia", 1000 },
+                {"duza", 10000 }
+            };
+            foreach(KeyValuePair<string, int> element in elements)
+            {
+                Console.WriteLine("Progressing...");
+                Console.WriteLine("If it taking too much time, you can decrease number of elements");
+                Console.WriteLine();
+                try
+                {
+                    _controllers.DataArrays.Clear();
+                    RunAlgo(element);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Oppps ! Something went Wrong !");
+                }
+
+            }
         }
 
 
-        private void RunAlgo()
+        private void RunAlgo(KeyValuePair<string, int> el)
         {
-            var controllers = new Controllers();
-            controllers.InitializeArrays(10);
-
             var AlgoImplementations = new AlgorithmsImplenentations();
 
-            foreach (var arr in controllers.RandomArrays)
+            _controllers.InitializeArrays(el.Value);
+            foreach (KeyValuePair<string, List<int[]>> data in _controllers.DataArrays)
             {
-                var sw = new Stopwatch();
-                sw.Start();
-                AlgoImplementations.InsertionSort(arr);
-                sw.Stop();
-                InsertionSort.Add(Math.Round(sw.Elapsed.TotalMilliseconds, 8));
-                sw.Restart();
-                AlgoImplementations.QuickSort(arr);
-                sw.Stop();
-                QuickSort.Add(Math.Round(sw.Elapsed.TotalMilliseconds, 8));
-                sw.Restart();
-                AlgoImplementations.MergeSort(arr, 0, arr.Length-1);
-                sw.Stop();
-                MergeSort.Add(Math.Round(sw.Elapsed.TotalMilliseconds, 8));
-                sw.Restart();
-                Array.Sort(arr);
-                sw.Stop();
-                QuickSortClassical.Add(Math.Round(sw.Elapsed.TotalMilliseconds, 8));
+                count++;
+                foreach (var arr in data.Value)
+                {
+                    var sw = new Stopwatch();
+                    sw.Start();
+                    AlgoImplementations.InsertionSort(arr);
+                    sw.Stop();
+                    InsertionSort.Add(Math.Round(sw.Elapsed.TotalMilliseconds, 8));
+                    sw.Restart();
+                    AlgoImplementations.QuickSort(arr);
+                    sw.Stop();
+                    QuickSort.Add(Math.Round(sw.Elapsed.TotalMilliseconds, 8));
+                    sw.Restart();
+                    AlgoImplementations.MergeSort(arr, 0, arr.Length - 1);
+                    sw.Stop();
+                    MergeSort.Add(Math.Round(sw.Elapsed.TotalMilliseconds, 8));
+                    sw.Restart();
+                    Array.Sort(arr);
+                    sw.Stop();
+                    QuickSortClassical.Add(Math.Round(sw.Elapsed.TotalMilliseconds, 8));
+                }
+                PrintEffects(count, el, data.Key);
+                Console.WriteLine();
+                Console.WriteLine();
             }
-
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine();
         }
 
-        private void PrintEffects()
+        private void PrintEffects(int count, KeyValuePair<string, int> el, string arrType)
         {
+            Console.WriteLine($"Przypadek {count}: próba {el.Key} (n = {el.Value}), {arrType}");
+            Console.WriteLine($"InsertionSort: t = {InsertionSort.Average()}");
+            Console.WriteLine($"MergeSort: t = {MergeSort.Average()}");
+            Console.WriteLine($"QuickSort: t = {QuickSort.Average()}");
+            Console.WriteLine($"QuickSortClassical: t = {QuickSortClassical.Average()}");
 
         }
     }
